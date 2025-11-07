555bbad0a9c7501ddd78c7c3703100f78059a9a5
Academic Purposes <lazarokirbyandrei.c@gmail.com>
2025-11-07 08:12:00 +0800

    Add project files.

---
+++ EDP-LabExe_10/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel = New System.Windows.Forms.TableLayoutPanel()
+        Me.ButtonRead = New System.Windows.Forms.Button()
+        Me.ButtonSave = New System.Windows.Forms.Button()
+        Me.ButtonInput = New System.Windows.Forms.Button()
+        Me.TextBoxNumberInput = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListBoxNumbers = New System.Windows.Forms.ListBox()
+        Me.TableLayoutPanel.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel
+        '
+        Me.TableLayoutPanel.ColumnCount = 3
+        Me.TableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel.Controls.Add(Me.ButtonRead, 1, 2)
+        Me.TableLayoutPanel.Controls.Add(Me.ButtonSave, 0, 2)
+        Me.TableLayoutPanel.Controls.Add(Me.ButtonInput, 1, 0)
+        Me.TableLayoutPanel.Controls.Add(Me.TextBoxNumberInput, 0, 1)
+        Me.TableLayoutPanel.Controls.Add(Me.Label1, 0, 0)
+        Me.TableLayoutPanel.Controls.Add(Me.ListBoxNumbers, 2, 0)
+        Me.TableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel.Name = "TableLayoutPanel"
+        Me.TableLayoutPanel.RowCount = 3
+        Me.TableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel.Size = New System.Drawing.Size(800, 450)
+        Me.TableLayoutPanel.TabIndex = 0
+        '
+        'ButtonRead
+        '
+        Me.ButtonRead.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ButtonRead.Location = New System.Drawing.Point(269, 303)
+        Me.ButtonRead.Name = "ButtonRead"
+        Me.ButtonRead.Size = New System.Drawing.Size(260, 144)
+        Me.ButtonRead.TabIndex = 5
+        Me.ButtonRead.Text = "Read"
+        Me.ButtonRead.UseVisualStyleBackColor = True
+        '
+        'ButtonSave
+        '
+        Me.ButtonSave.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ButtonSave.Location = New System.Drawing.Point(3, 303)
+        Me.ButtonSave.Name = "ButtonSave"
+        Me.ButtonSave.Size = New System.Drawing.Size(260, 144)
+        Me.ButtonSave.TabIndex = 4
+        Me.ButtonSave.Text = "Save"
+        Me.ButtonSave.UseVisualStyleBackColor = True
+        '
+        'ButtonInput
+        '
+        Me.ButtonInput.AutoSize = True
+        Me.ButtonInput.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ButtonInput.Location = New System.Drawing.Point(269, 3)
+        Me.ButtonInput.Name = "ButtonInput"
+        Me.ButtonInput.Size = New System.Drawing.Size(260, 144)
+        Me.ButtonInput.TabIndex = 1
+        Me.ButtonInput.Text = "Next Input"
+        Me.ButtonInput.UseVisualStyleBackColor = True
+        '
+        'TextBoxNumberInput
+        '
+        Me.TableLayoutPanel.SetColumnSpan(Me.TextBoxNumberInput, 2)
+        Me.TextBoxNumberInput.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBoxNumberInput.Location = New System.Drawing.Point(3, 153)
+        Me.TextBoxNumberInput.Name = "TextBoxNumberInput"
+        Me.TextBoxNumberInput.Size = New System.Drawing.Size(526, 20)
+        Me.TextBoxNumberInput.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(3, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(260, 150)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Enter Numbers"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'ListBoxNumbers
+        '
+        Me.ListBoxNumbers.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ListBoxNumbers.FormattingEnabled = True
+        Me.ListBoxNumbers.Location = New System.Drawing.Point(535, 3)
+        Me.ListBoxNumbers.Name = "ListBoxNumbers"
+        Me.TableLayoutPanel.SetRowSpan(Me.ListBoxNumbers, 3)
+        Me.ListBoxNumbers.Size = New System.Drawing.Size(262, 444)
+        Me.ListBoxNumbers.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TableLayoutPanel)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TableLayoutPanel.ResumeLayout(False)
+        Me.TableLayoutPanel.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel As TableLayoutPanel
+    Friend WithEvents TextBoxNumberInput As TextBox
+    Friend WithEvents ButtonInput As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ButtonRead As Button
+    Friend WithEvents ButtonSave As Button
+    Friend WithEvents ListBoxNumbers As ListBox
+End Class
---
+++ EDP-LabExe_10/Form1.vb
@@ -0,0 +1,52 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub ButtonInput_Click(sender As Object, e As EventArgs) Handles ButtonInput.Click
+
+        If Not TextBoxNumberInput.Text.Trim() = "" Then
+            Dim num As Integer
+            If Integer.TryParse(TextBoxNumberInput.Text.Trim(), num) Then
+                ListBoxNumbers.Items.Add(num)
+                TextBoxNumberInput.Clear()
+            Else
+                MessageBox.Show("Please enter a valid number.")
+            End If
+        Else
+            MessageBox.Show("Please enter a number first.")
+        End If
+    End Sub
+    '======================================================================================================
+    Private Sub ButtonSave_Click(sender As Object, e As EventArgs) Handles ButtonSave.Click
+        Dim filePath As String = "numbers.txt"
+
+        If ListBoxNumbers.Items.Count > 0 Then
+            Using writer As New StreamWriter(filePath)
+                ListBoxNumbers.Items.Cast(Of Integer)().ToList().ForEach(Sub(num) writer.Write(num & " "))
+            End Using
+            ListBoxNumbers.Items.Clear()
+            MessageBox.Show("Numbers saved to " & filePath)
+        Else
+            MessageBox.Show("No numbers to save.")
+        End If
+    End Sub
+    '======================================================================================================
+    Private Sub ButtonRead_Click(sender As Object, e As EventArgs) Handles ButtonRead.Click
+        Dim filePath As String = "numbers.txt"
+
+        If File.Exists(filePath) Then
+            ListBoxNumbers.Items.Clear()
+            Using reader As New StreamReader(filePath)
+                Dim line As String = reader.ReadLine()
+                If Not String.IsNullOrEmpty(line) Then
+                    Dim parts() As String = line.Split(" "c)
+                    Dim numberList = parts.Where(Function(p) p.Trim() <> "").Select(Function(p) Integer.Parse(p)).OrderBy(Function(x) x)
+                    numberList.ToList().ForEach(Sub(num) ListBoxNumbers.Items.Add(num))
+                End If
+            End Using
+
+            MessageBox.Show("Numbers loaded successfully!")
+        Else
+            MessageBox.Show("File not found! Please save numbers first.")
+        End If
+    End Sub
+End Class
